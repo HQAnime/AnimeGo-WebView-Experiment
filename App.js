52141ea71d0e38dac873128e62c258d2bc6ec808
Yiheng (NUC) <development.henryquan@gmail.com>
2020-01-21 22:00:39 +1100

    added a function to get domain from link

--- App.js
+++ App.js
@@ -11,19 +11,44 @@ import { View, Text, ToastAndroid } from 'react-native';
 import WebView from 'react-native-webview';
 
 const App = () => {
+  let link = 'https://www.fembed.com/v/j-5gdcdq-nwldn2';
+  let domain = getDomainNameFromLink(link);
+  console.log(domain);
+
   return (
     <View style={{flex: 1}}>
-      <WebView source={{ uri: 'https://vidstreaming.io/streaming.php?id=MTM1ODQx&title=BanG+Dream%21+3rd+Season+episode+4&typesub=SUB' }}
+      <WebView source={{ uri: link }}
         allowsFullscreenVideo
         onShouldStartLoadWithRequest={request => {
           // Only allow navigating within this website
           console.log(request);
-          ToastAndroid.showWithGravity('Popup has been blocked', 0.5, ToastAndroid.TOP);
-          return request.url.startsWith('https://facebook.github.io/react-native');
+
+          const { url, title } = request;
+          let shouldLoad = url.includes(domain) || title === '';
+          if (!shouldLoad) {
+            ToastAndroid.showWithGravity('Popup has been blocked', 0.5, ToastAndroid.TOP);
+          }
+
+          return shouldLoad;
         }} />
     </View>
   );
 };
+
+/**
+ * Consider these cases
+ * - https://www.google.com/
+ * - https://google.com/
+ * - google.com
+ * - google.com/sdsahjs
+ * First, we need to remove everything before //
+ * @param {String} link 
+ */
+function getDomainNameFromLink(link) {
+  let domain = link.split('//').pop();
+  domain = domain.split('/').shift();
+  return domain.split('www.').pop();
+}
 
 
 export default App;
