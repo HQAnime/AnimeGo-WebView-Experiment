640eada24cd55fd2548b3be41cd413adc1989f24
Yiheng (NUC) <development.henryquan@gmail.com>
2020-01-21 22:47:56 +1100

    added react-native-paper

--- Experiment/index.js
+++ Experiment/index.js
@@ -2,8 +2,18 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import * as React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
